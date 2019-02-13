f12fe3cbe80d57a9cd298c7d2f3263110ca93660
Nicolai Andriuta <nickKolia@gmail.com>
2019-02-13 15:16:05 +0200

    HW3

---
+++ Andriuta/lesson3/homework_lesson_3.rb
@@ -0,0 +1,31 @@
+a=-123
+puts a%1==0 && a.to_i.even? 
+puts a%1==0 && a.to_i.odd?
+
+-123.odd?
+
+puts "The next number to 6893 will be #{6893+1}"
+
+puts "The previous number to 0 is #{0-1}"
+
+(12.to_i/ 4.5).round
+
+(28%6) * 4.7
+18.8.floor
+
+puts "Brand new bicycle\r\n".to_sym
+
+puts "Brand new bicycle"[6..9]
+
+puts "Brand new bicycle".sub("bicycle", " ")
+
+my_string = "Brand new bicycle"
+if my_string.include? "bicycle"
+puts "String includes 'bicycle'"
+end
+
+"Brand new bicycle".index("w")
+
+puts "I thought I knew \neverything \nbefore that night"
+
+"Final result from Float/Integer part is:#{-123-1+6894+3+18}"
